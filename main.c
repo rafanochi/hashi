b7da6da10ea2b646c3bd1e74c63e10210b67b5fd
rafanochi <zawkindev@gmail.com>
2025-11-27 00:19:46 +0500

    create new socket and bind it

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "netinet/in.h"
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT 8080
+
+int main() {
+
+  // create the socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    perror("couldn't create a socket!\n");
+    return 1;
+  }
+  printf("socket created successfully\n");
+
+  struct sockaddr_in host_addr;
+  int host_addrlen = sizeof(host_addr);
+
+  host_addr.sin_family = AF_INET;
+  host_addr.sin_port = htons(PORT);
+  host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+    perror("couldn't bing the socket!\n");
+    return 1;
+  }
+  printf("socket BINDED successfully\n");
+
+  return 0;
+}
