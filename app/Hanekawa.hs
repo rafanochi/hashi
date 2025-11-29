5c93297a6c17d725fa07a62f526643470d57e175
rafanochi <zawkindev@gmail.com>
2025-11-29 23:33:08 +0500

    almost finish the protocol

---
+++ app/Hanekawa.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+-- Custom ultimately simple Protocol
+module Hanekawa where
+
+import Data.Aeson
+import GHC.Generics
+
+data Status = Ok | Error
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+data Response = Response
+  { status :: Status
+  , result :: Maybe Value
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+data Request = Request
+  { method :: String
+  , params :: Maybe Value
+  }
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON, ToJSON)
+
+class (ToJSON a, FromJSON a) => Hanekawa a where
+  wrap :: Maybe a -> Status -> Response
+  wrap Nothing s = Response{status = s, result = Nothing}
+  wrap (Just res) s = Response{status = s, result = Just (toJSON res)}
+
+  unwrap :: Request -> (String, Maybe a)
