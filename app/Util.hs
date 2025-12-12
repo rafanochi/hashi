92ebaabfd4c8451301b6ebf6bbcfc2325e830c05
rafanochi <zawkindev@gmail.com>
2025-12-12 12:01:19 +0500

    trying

--- app/Util.hs
+++ app/Util.hs
@@ -1,6 +1,9 @@
 module Util where
 
-import DB (Hub (hubCity), HubConnection (hubConnectionFromHub, hubConnectionToHub))
+import DB (Hub (hubCity), HubConnection (hubConnectionDistance, hubConnectionFromHub, hubConnectionToHub))
+
+instance Eq HubConnection where
+  a == b = hubConnectionDistance a == hubConnectionDistance b
 
 instance Ord HubConnection where
   compare a b = compare (hubConnectionDistance a) (hubConnectionDistance b)
@@ -9,15 +12,23 @@ instance Ord HubConnection where
 (=?) :: Hub -> Hub -> Bool
 a =? b = hubCity a == hubCity b
 
-(~) :: [HubConnection] -> [HubConnection] -> [HubConnection]
-x ~ y = max x y
+onlyHubs :: [HubConnection] -> [Hub]
+onlyHubs [] = []
+onlyHubs [x] = [hubConnectionToHub x]
+onlyHubs (x : xs) = hubConnectionFromHub x : onlyHubs xs
 
-short :: [HubConnection] -> (Hub, Hub) -> [Hub]
-short edges (from, to) = 
-    where 
-        that :: Hub -> [HubConnection] -> [HubConnection] -> [Hub]
-        that hub hubcs state = case length $ filter (\y->hubConnectionToHub y =? to) $ 
-            filter (\x-> hubConnectionFromHub x =? hub) hubcs of
-                1 -> state 
-                _ -> 
-                
+getPath :: Hub -> HubConnection -> Bool
+getPath node edge = 
+
+
+that :: Hub -> [HubConnection] -> [HubConnection]
+that node edges state =  
+
+-- short :: [HubConnection] -> (Hub, Hub) -> [Hub]
+-- short edges (from, to) =
+--     where
+--         that :: Hub -> [HubConnection] -> [HubConnection] -> [Hub]
+--         that hub hubcs state = case length $ filter (\y->hubConnectionToHub y =? to) $
+--             filter (\x-> hubConnectionFromHub x =? hub) hubcs of
+--                 1 -> state
+--                 _ ->
