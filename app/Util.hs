203df48d3541ac9ad962c2662dc312024a7fe88b
rafanochi <zawkindev@gmail.com>
2025-12-12 10:46:34 +0500

    crazy implementation of Ord for HubConnection

--- app/Util.hs
+++ app/Util.hs
@@ -2,10 +2,22 @@ module Util where
 
 import DB (Hub (hubCity), HubConnection (hubConnectionFromHub, hubConnectionToHub))
 
+instance Ord HubConnection where
+  compare a b = compare (hubConnectionDistance a) (hubConnectionDistance b)
+
 -- check if the city is same
-(=?=) :: Hub -> Hub -> Bool
-a =?= b = hubCity a == hubCity b
+(=?) :: Hub -> Hub -> Bool
+a =? b = hubCity a == hubCity b
+
+(~) :: [HubConnection] -> [HubConnection] -> [HubConnection]
+x ~ y = max x y
 
 short :: [HubConnection] -> (Hub, Hub) -> [Hub]
-short edges (from, to) =
-  (filter (\x -> hubConnectionFromHub x =?= from) edges)
+short edges (from, to) = 
+    where 
+        that :: Hub -> [HubConnection] -> [HubConnection] -> [Hub]
+        that hub hubcs state = case length $ filter (\y->hubConnectionToHub y =? to) $ 
+            filter (\x-> hubConnectionFromHub x =? hub) hubcs of
+                1 -> state 
+                _ -> 
+                
